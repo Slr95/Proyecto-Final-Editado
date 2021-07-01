212760bff7f1c111b9ed313f021307108a2ab79f
benjhi <renzo_soler@hotmail.com>
2021-07-01 18:25:12 -0300

    master

---
+++ js/codigo.js
@@ -0,0 +1,12 @@
+function abrir(){
+
+    document.getElementById('formulario').style.display = 'block'
+    
+}
+    
+function cerrar(){
+    
+    document.getElementById('formulario').style.display = 'none'
+        
+        
+}
